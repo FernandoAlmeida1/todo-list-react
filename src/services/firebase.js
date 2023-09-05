1b4ae37970121aa92070fdf7c318efa596455819
Fernando Sa de Almeida <fernando.sa@itriad.org.br>
2023-09-05 10:02:20 -0400

    [IMPROVING] project todo list

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,6 @@ import {
   collection,
   addDoc,
   query,
-  where,
   doc,
   updateDoc,
   deleteDoc,
@@ -13,56 +12,12 @@ const COLLECTION_LIST_NAME = "TodoList";
 const COLLECTION_TASK_NAME = "task";
 const COLLECTION_USERS_NAME = "users";
 
-export const findAllLists = (user) => {
-  return query(
-    collection(db, COLLECTION_LIST_NAME),
-    where("users", "array-contains", user)
-  );
-};
-export const findAllListsUsers = (id) => {
-  return query(doc(db, COLLECTION_LIST_NAME, id));
-};
-
-export const findAllTasks = (id) => {
-  return query(collection(db, COLLECTION_LIST_NAME, id, COLLECTION_TASK_NAME));
-};
-
-export const createList = async (body) => {
-  return await addDoc(collection(db, COLLECTION_LIST_NAME), body);
-};
-
 export const addTask = async (body) => {
   return await addDoc(collection(db, COLLECTION_LIST_NAME), body);
 };
 
-export const getUser = (email) => {
-  return query(
-    collection(db, COLLECTION_USERS_NAME),
-    where("email", "==", email)
-  );
-};
-
-export const createUser = async (body) => {
-  return await addDoc(collection(db, COLLECTION_USERS_NAME), body);
-};
-
 export const addUsersToList = async (listId, body) => {
   return await updateDoc(doc(db, COLLECTION_LIST_NAME, listId), body);
-};
-
-export const updateList = async (listId, body) => {
-  return await updateDoc(doc(db, COLLECTION_LIST_NAME, listId), body);
-};
-
-export const updateTask = async (listId, taskId, body) => {
-  return await updateDoc(
-    doc(db, COLLECTION_LIST_NAME, listId, COLLECTION_TASK_NAME, taskId),
-    body
-  );
-};
-
-export const deleteList = async (listId) => {
-  return await deleteDoc(doc(db, COLLECTION_LIST_NAME, listId));
 };
 
 export const deleteTask = async (listId, taskId) => {
@@ -80,13 +35,13 @@ export const removeTask = async (taskId) => {
 };
 
 export const editTask = async (taskId, body) => {
-  return await updateDoc(doc(db, COLLECTION_LIST_NAME, taskId), body)
-}
+  return await updateDoc(doc(db, COLLECTION_LIST_NAME, taskId), body);
+};
 
 export const addUser = async (body) => {
-  return await addDoc(collection(db, COLLECTION_USERS_NAME), body)
-}
+  return await addDoc(collection(db, COLLECTION_USERS_NAME), body);
+};
 
 export const getAllUsers = () => {
-  return query(collection(db, COLLECTION_USERS_NAME))
-}
+  return query(collection(db, COLLECTION_USERS_NAME));
+};
